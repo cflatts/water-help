811ba3ef1460ae1c3339f0e33d9473abb6671141
Caitlin Flattery <caitlinflattery@gmail.com>
2017-02-08 12:52:32 -0600

    created empty components and imported react router to App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Router, Route, browserHistory, IndexRoute} from 'react-router'
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,17 +7,7 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return
   }
 }
 
