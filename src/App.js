6574142d6143c318d06b078ed7b7019e88261c08
Caitlin Flattery <caitlinflattery@gmail.com>
2017-02-08 13:23:35 -0600

    created nav links for navigation so nav bar is on every component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Router, Route, browserHistory, IndexRoute} from 'react-router'
 import Home from './components/home'
 import Graph from './components/graph'
 import Login from './components/login'
+import Navigation from './components/navigation'
 import logo from './logo.svg';
 import './App.css';
 
@@ -12,6 +13,7 @@ class App extends Component {
   render() {
     return (
         <Router history = {browserHistory}>
+            <Route path = '/' component = {Navigation}/>
             <Route path = '/home' component = {Home} />
             <Route path = '/graph' component = {Graph} />
             <Route path = 'login' component = {Login} />
---
+++ src/components/navigation.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import NavLink from './NavLink'
+
+class Navigation extends Component {
+    render() {
+        return (
+            <div className = 'navigation'>
+                <NavLink to = '/'>Home</NavLink>
+                <NavLink to = '/graph'>Water Goals</NavLink>
+                <NavLink to = '/login'>Login</NavLink>
+            </div>
+            )
+    }
+}
+
+export default Navigation
