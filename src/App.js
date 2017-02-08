13d89eac550b332bd4d2b6a02e49e36f0f225c57
Caitlin Flattery <caitlinflattery@gmail.com>
2017-02-08 12:56:19 -0600

    imported components to App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import {Router, Route, browserHistory, IndexRoute} from 'react-router'
+import Home from './components/home'
+import Graph from './components/graph'
+import Login from './components/login'
 import logo from './logo.svg';
 import './App.css';
 
